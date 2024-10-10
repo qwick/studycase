8c4306743e487c53716b1ad87183c7dae63561fb
qwick <1014080209@qq.com>
2024-10-10 21:48:21 +0800

    增加停车场案例

--- src/test/java/com/shenlan/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/shenlan/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.shenlan.springboot;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class SpringbootApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.shenlan.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
